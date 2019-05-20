8cf1ab4e3fdc69569e3fff02ae86e55a789803c8
Eric Tobin <eatobin@gmail.com>
2019-05-20 17:00:33 -0500

    all working - no #lang

--- test/borrower-test.rkt
+++ test/borrower-test.rkt
@@ -1,10 +1,9 @@
-(module borrower-test racket)
-
-(require rackunit
+(module borrower-test racket
+  (require rackunit
          "../src/borrower.rkt")
-
-(define br1 (make-borrower "borrower1" 1))
-
-(check-equal? (get-name br1) "borrower1")
-
-(check-equal? (set-name br1 "joey") (borrower "joey" 1))
+  
+  (define br1 (make-borrower "borrower1" 1))
+  
+  (check-equal? (get-name br1) "borrower1")
+  
+  (check-equal? (set-name br1 "joey") (borrower "joey" 1)))
