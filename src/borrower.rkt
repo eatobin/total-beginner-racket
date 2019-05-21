60b2d8049d9076cfad934a4638c6929968caaef7
Eric Tobin <eatobin@gmail.com>
2019-05-21 13:18:06 -0500

    dev working

--- src/borrower.rkt
+++ src/borrower.rkt
@@ -1,29 +1,37 @@
 ; ,cd "/home/eric/lisp_projects/scheme/racket/total-racket"
-; ,enter "test/borrower.rkt"
+; ,enter "src/borrower.rkt"
 
-(module borrower racket
+#lang racket
+
+(provide (struct-out borrower)
+         (all-defined-out))
+
+(struct borrower (name max-books) #:transparent)
+
+(define (make-borrower n mb)
+  (borrower n mb))
+
+(define (get-name br)
+  (borrower-name br))
+
+(define (set-name br n)
+  (struct-copy borrower br [name n]))
+
+(define get-max-books
+  (lambda (br)
+    (borrower-max-books br)))
   
-  (provide (struct-out borrower))
-  (provide (all-defined-out))
-
-  (struct borrower (name max-books) #:transparent)
-
-  (define (make-borrower n mb)
-    (borrower n mb))
-
-  (define (get-name br)
-    (borrower-name br))
-
-  (define (set-name br n)
-    (struct-copy borrower br [name n]))
-
-  (define get-max-books
-    (lambda (br)
-      (borrower-max-books br)))
+(define set-max-books
+  (lambda (br mb)
+    (struct-copy borrower br [max-books mb])))
   
-  (define set-max-books
-    (lambda (br mb)
-      (struct-copy borrower br [max-books mb])))
-  
-  (define borrower-to-string
-    "do this"))
+(define borrower-to-string
+  "do this")
+ 
+(module* test #f
+  (require rackunit)
+  (define br1 (make-borrower "borrower1" 1))
+  (check-equal? (get-name br1) "borrower1")
+  (check-equal? (set-name br1 "joey") (borrower "joey" 1))
+  (check-equal? (get-max-books br1) 1)
+  (check-equal? (set-max-books br1 11) (borrower "borrower1" 11)))
