6703401d1545aea49b4794d9bd186cde0b5e36e6
Eric Tobin <eatobin@gmail.com>
2019-06-30 20:03:18 -0500

    clean up

--- book.rkt
+++ book.rkt
@@ -21,15 +21,6 @@
 (define (get-borrower book)
   (hash-ref book 'm-borrower))
 
-;; (define (set-name borrower name)
-;;   (hash-update borrower 'name (lambda (_) name)))
-
-;; (define (get-max-books borrower)
-;;   (hash-ref borrower 'max-books))
-
-;; (define (set-max-books borrower max-books)
-;;   (hash-update borrower 'max-books (lambda (_) max-books)))
-
 ;; (define (borrower-to-string borrower)
 ;;   (string-append (get-name borrower) " (" (number->string (get-max-books borrower)) " books)"))
 
@@ -50,9 +41,5 @@
       (check-equal? (get-author bk1) "Author1")
       (check-equal? (get-borrower bk1) #f)
       (check-equal? (get-borrower bk2) br2)))
-      ;; (check-equal? (set-name br1 "joey") (make-borrower "joey" 1))
-      ;; (check-equal? (get-max-books br1) 1)
-      ;; (check-equal? (set-max-books br1 10) (make-borrower "borrower1" 10))
-      ;; (check-equal? (borrower-to-string br1) "borrower1 (1 books)")))
 
   (run-tests file-tests))
