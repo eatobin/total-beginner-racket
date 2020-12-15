5d6666201ed5c6ad9f9121d813a36f9029c40242
Eric Tobin <eatobin@gmail.com>
2020-12-14 21:28:55 -0600

    book tests

--- book.rkt
+++ book.rkt
@@ -8,35 +8,27 @@
 (require struct-update
          "borrower.rkt")
 
-(provide (struct+updaters-out book))
+(provide (struct+updaters-out book)
+         book-to-string)
 
 (struct book (title author maybe-borrower) #:transparent)
 (define-struct-updaters book)
 
-(define (set-title bk ttl)
-  (book-title-set bk ttl))
+(define (available-string book)
+  (let ([borrower (book-maybe-borrower book)])
+    (if (eq? borrower 'null)
+        "Available"
+        (string-append
+         "Checked out to "
+         (borrower-name borrower)))))
 
-(define (set-author bk aut)
-  (book-author-set bk aut))
-
-(define (set-borrower bk mbr)
-  (book-maybe-borrower-set bk mbr))
-
- (define (available-string book)
-   (let ([borrower (book-maybe-borrower book)])
-     (if (eq? borrower 'null)
-         "Available"
-         (string-append
-          "Checked out to "
-          (borrower-name borrower)))))
-
- (define (book-to-string book)
-   (string-append
-    (book-title book)
-    " by "
-    (book-author book)
-    "; "
-    (available-string book)))
+(define (book-to-string book)
+  (string-append
+   (book-title book)
+   " by "
+   (book-author book)
+   "; "
+   (available-string book)))
 
 ;; Tests
 (module* test #f
@@ -46,29 +38,29 @@
   (define br2 (borrower "Borrower2" 2))
   (define bk1 (book "Title2" "Author2" 'null))
   (define bk2 (book "Title2" "Author2" br2))
+  (define bad-title (book "Nope" "Author2" 'null))
+  (define bad-author (book "Title2" "Nope" 'null))
+  (define bk-str-avail "Title2 by Author2; Available")
+  (define bk-str-out "Title2 by Author2; Checked out to Borrower2")
 
   (define file-tests
     (test-suite
      "Tests for book.rkt"
 
      (test-case
-      "Book has the correct title"
-      (check-equal? (book-title bk1) "Title2"))
+      "Book has the correct avail string"
+      (check-equal? (book-to-string bk1) bk-str-avail))
      (test-case
-      "Book has the correct author"
-      (check-equal? (book-author bk1) "Author2"))
+      "Book has the correct out string"
+      (check-equal? (book-to-string bk2) bk-str-out))
      (test-case
-      "Book has the correct null borrower"
-      (check-equal? (book-maybe-borrower bk1) 'null))
+      "Book sets the correct title"
+      (check-equal? (book-to-string (book-title-set bad-title "Title2")) bk-str-avail))
      (test-case
-      "Book has the correct borrower"
-      (define new-bk (set-borrower bk1 br2))
-      (check-equal? bk2 new-bk))))
-  ;      (check-equal? (get-borrower bk2) br2)
-  ;      (check-equal? (set-title bk1 "Norman") (make-book "Norman" "Author1"))
-  ;      (check-equal? (set-author bk1 "Wow") (make-book "Title1" "Wow"))
-  ;      (check-equal? (set-borrower bk1 (make-borrower "Borrower99" 99)) (make-book "Title1" "Author1" (make-borrower "Borrower99" 99)))
-  ;      (check-equal? (book-to-string bk1) "Title1 by Author1; Available")
-  ;      (check-equal? (book-to-string bk2) "Title2 by Author2; Checked out to Borrower2")))
+      "Book sets the correct author"
+      (check-equal? (book-to-string (book-author-set bad-author "Author2")) bk-str-avail))
+     (test-case
+      "Book sets the correct borrower"
+      (check-equal? (book-to-string (book-maybe-borrower-set bk1 br2)) bk-str-out))))
 
   (run-tests file-tests))
