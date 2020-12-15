abc180e2d7d6c7e23ccdcf173893ed4759095631
Eric Tobin <eatobin@gmail.com>
2020-12-14 20:47:00 -0600

    struct progress

--- book.rkt
+++ book.rkt
@@ -8,7 +8,7 @@
 (require struct-update
          "borrower.rkt")
 
-(provide (struct-updaters-out book))
+(provide (struct+updaters-out book))
 
 (struct book (title author maybe-borrower) #:transparent)
 (define-struct-updaters book)
@@ -22,21 +22,21 @@
 (define (set-borrower bk mbr)
   (book-maybe-borrower-set bk mbr))
 
-                                        ; (define (available-string book)
-                                        ;   (let ([borrower (get-borrower book)])
-                                        ;     (if (eq? borrower 'null)
-                                        ;         "Available"
-                                        ;         (string-append
-                                        ;          "Checked out to "
-                                        ;          (get-name borrower)))))
+ (define (available-string book)
+   (let ([borrower (book-maybe-borrower book)])
+     (if (eq? borrower 'null)
+         "Available"
+         (string-append
+          "Checked out to "
+          (borrower-name borrower)))))
 
-                                        ; (define (book-to-string book)
-                                        ;   (string-append
-                                        ;    (get-title book)
-                                        ;    " by "
-                                        ;    (get-author book)
-                                        ;    "; "
-                                        ;    (available-string book)))
+ (define (book-to-string book)
+   (string-append
+    (book-title book)
+    " by "
+    (book-author book)
+    "; "
+    (available-string book)))
 
 ;; Tests
 (module* test #f
@@ -64,11 +64,11 @@
       "Book has the correct borrower"
       (define new-bk (set-borrower bk1 br2))
       (check-equal? bk2 new-bk))))
-                                        ;      (check-equal? (get-borrower bk2) br2)
-                                        ;      (check-equal? (set-title bk1 "Norman") (make-book "Norman" "Author1"))
-                                        ;      (check-equal? (set-author bk1 "Wow") (make-book "Title1" "Wow"))
-                                        ;      (check-equal? (set-borrower bk1 (make-borrower "Borrower99" 99)) (make-book "Title1" "Author1" (make-borrower "Borrower99" 99)))
-                                        ;      (check-equal? (book-to-string bk1) "Title1 by Author1; Available")
-                                        ;      (check-equal? (book-to-string bk2) "Title2 by Author2; Checked out to Borrower2")))
+  ;      (check-equal? (get-borrower bk2) br2)
+  ;      (check-equal? (set-title bk1 "Norman") (make-book "Norman" "Author1"))
+  ;      (check-equal? (set-author bk1 "Wow") (make-book "Title1" "Wow"))
+  ;      (check-equal? (set-borrower bk1 (make-borrower "Borrower99" 99)) (make-book "Title1" "Author1" (make-borrower "Borrower99" 99)))
+  ;      (check-equal? (book-to-string bk1) "Title1 by Author1; Available")
+  ;      (check-equal? (book-to-string bk2) "Title2 by Author2; Checked out to Borrower2")))
 
   (run-tests file-tests))
--- borrower.rkt
+++ borrower.rkt
@@ -6,19 +6,11 @@
 
 (require struct-update)
 
-(provide (struct-updaters-out borrower)
-         set-name
-         set-max-books
+(provide (struct+updaters-out borrower)
          borrower-to-string)
 
 (struct borrower (name max-books) #:transparent)
 (define-struct-updaters borrower)
-
-(define (set-name br n)
-  (borrower-name-set br n))
-
-(define (set-max-books br mb)
-  (borrower-max-books-set br mb))
 
 (define (borrower-to-string br)
   (string-append (borrower-name br) " (" (number->string (borrower-max-books br)) " books)"))
@@ -29,25 +21,21 @@
            rackunit/text-ui)
 
   (define br1 (borrower "Borrower1" 1))
+  (define bad-name (borrower "Jack" 1))
+  (define bad-mb (borrower "Borrower1" 11))
+  (define br-str "Borrower1 (1 books)")
 
   (define file-tests
     (test-suite
      "Tests for borrower.rkt"
 
      (test-case
-      "Borrower has the correct name"
-      (check-equal? (borrower-name br1) "Borrower1"))
+      "Borrower prints the correct borrower to string"
+      (check-equal? (borrower-to-string br1) br-str))
      (test-case
       "Borrower sets the correct name"
-      (check-equal? (set-name br1 "Joey") (borrower "Joey" 1)))
-     (test-case
-      "Borrower has the correct max-books"
-      (check-equal? (borrower-max-books br1) 1))
-     (test-case
-      "Borrower sets the correct max-books"
-      (check-equal? (set-max-books br1 10) (borrower "Borrower1" 10)))
-     (test-case
-      "Borrower prints the correct borrower to string"
-      (check-equal? (borrower-to-string br1) "Borrower1 (1 books)"))))
+      (check-equal? (borrower-to-string (borrower-name-set bad-name "Borrower1")) br-str))
+     "Borrower sets the correct max-books"
+     (check-equal? (borrower-to-string (borrower-max-books-set bad-mb 1)) br-str)))
 
   (run-tests file-tests))
