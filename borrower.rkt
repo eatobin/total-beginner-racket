fb257100ae6ed57b2fb0b69a6a53881676788721
Eric Tobin <eatobin@gmail.com>
2022-02-22 21:43:13 -0800

    works!!!

--- borrower.rkt
+++ borrower.rkt
@@ -1,21 +1,29 @@
+;; ,cd "~/lisp_projects/scheme/total-racket"
+;; ,enter "borrower.rkt"
+;; [eric@linux-x2vq total-racket](master)$ raco test "borrower.rkt"
+
 #lang racket
+
 (module+ test
-  (require rackunit)
-  (define ε 1e-10))
- 
-(provide drop
-         to-energy)
- 
-(define (drop t)
-  (* 1/2 9.8 t t))
- 
+  )
+(require fmt
+         rackunit
+         struct-update)
+
+(provide (struct+updaters-out borrower)
+         borrower-to-string)
+
+(struct borrower (name max-books) #:transparent)
+(define-struct-updaters borrower)
+
+(define (borrower-to-string br)
+  (string-append (borrower-name br) " (" (number->string (borrower-max-books br)) " books)"))
+
 (module+ test
-  (check-= (drop 0) 0 ε)
-  (check-= (drop 10) 490 ε))
- 
-(define (to-energy m)
-  (* m (expt 299792458.0 2)))
- 
-(module+ test
-  (check-= (to-energy 0) 0 ε)
-  (check-= (to-energy 1) 9e+16 1e+15))
+  (define br1 (borrower "Borrower1" 1))
+  (define br-str "Borrower1 (1 books)")
+  (define bad-name (borrower "Jack" 1))
+  (define bad-mb (borrower "Borrower1" 11))
+  (check-equal? (borrower-to-string br1) br-str)
+  (check-equal? (borrower-to-string (borrower-name-set bad-name "Borrower1")) br-str)
+  (check-equal? (borrower-to-string (borrower-max-books-set bad-mb 1)) br-str))
